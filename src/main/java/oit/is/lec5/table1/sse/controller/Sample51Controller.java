65e8c48dd6df5d2d997b4f899728e8ab3fcb7619
e1b20014 <e1b20014@oit.ac.jp>
2022-10-25 15:55:15 +0900

    購入処理

--- src/main/java/oit/is/lec5/table1/sse/controller/Sample51Controller.java
+++ src/main/java/oit/is/lec5/table1/sse/controller/Sample51Controller.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-// import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.lec5.table1.sse.model.Fruit;
 import oit.is.lec5.table1.sse.model.FruitMapper;
@@ -32,4 +32,21 @@ public class Sample51Controller {
     model.addAttribute("fruits2", fruits2);
     return "sample51.html";
   }
+
+  @GetMapping("step3")
+  @Transactional
+  public String sample53(@RequestParam Integer id, ModelMap model) {
+    // 削除対象のフルーツを取得
+    Fruit fruit3 = fMapper.selectById(id);
+    model.addAttribute("fruit3", fruit3);
+
+    // 削除
+    fMapper.deleteById(id);
+
+    // 削除後のフルーツリストを取得
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
+
 }
--- src/main/java/oit/is/lec5/table1/sse/model/FruitMapper.java
+++ src/main/java/oit/is/lec5/table1/sse/model/FruitMapper.java
@@ -2,7 +2,7 @@ package oit.is.lec5.table1.sse.model;
 
 import java.util.ArrayList;
 
-// import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 // import org.apache.ibatis.annotations.Update;
@@ -13,4 +13,12 @@ public interface FruitMapper {
   @Select("SELECT ID, NAME,PRICE FROM FRUIT")
   ArrayList<Fruit> selectAllFruit();
 
+  @Select("select ID, NAME,PRICE from FRUIT WHERE ID = #{id}")
+  Fruit selectById(int id);
+  
+  @Delete("DELETE FROM FRUIT WHERE ID =#{id}")
+  boolean deleteById(int id);
+
 }
+
+
