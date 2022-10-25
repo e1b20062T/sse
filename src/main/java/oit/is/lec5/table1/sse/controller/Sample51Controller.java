b9eb620994de6b34ffadfd6afa1269c568c096d4
syuumy5050 <osamigaki3856@gmail.com>
2022-10-25 15:46:06 +0900

    全ての果物のレコードを表示

--- src/main/java/oit/is/lec5/table1/sse/controller/Sample51Controller.java
+++ src/main/java/oit/is/lec5/table1/sse/controller/Sample51Controller.java
@@ -1,22 +1,35 @@
 package oit.is.lec5.table1.sse.controller;
 
-// import java.util.ArrayList;
+import java.util.ArrayList;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 // import org.springframework.transaction.annotation.Transactional;
-// import org.springframework.ui.ModelMap;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.lec5.table1.sse.model.Fruit;
+import oit.is.lec5.table1.sse.model.FruitMapper;
+
 @Controller
 @RequestMapping("/sample5")
 public class Sample51Controller {
 
+  @Autowired
+  FruitMapper fMapper;
+
   @GetMapping("step1")
   public String sample51() {
+    return "sample51.html";
+  }
+
+  @GetMapping("step2")
+  public String sample52(ModelMap model) {
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
     return "sample51.html";
   }
 }
