d909de62ab6cff802609d15d3467901fbcad0535
e1b20014 <e1b20014@oit.ac.jp>
2022-10-25 16:09:01 +0900

    果物データを編集する

--- src/main/java/oit/is/lec5/table1/sse/model/FruitMapper.java
+++ src/main/java/oit/is/lec5/table1/sse/model/FruitMapper.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-// import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface FruitMapper {
@@ -19,8 +19,8 @@ public interface FruitMapper {
   @Delete("DELETE FROM FRUIT WHERE ID =#{id}")
   boolean deleteById(int id);
 
-  //@Update("UPDATE FRUIT SET NAME=#{name}, PRICE=#{price} WHERE ID = #{id}")
-  //void updateById(Fruit fruit);
+  @Update("UPDATE FRUIT SET NAME=#{name}, PRICE=#{price} WHERE ID = #{id}")
+  void updateById(Fruit fruit);
 
 }
 
