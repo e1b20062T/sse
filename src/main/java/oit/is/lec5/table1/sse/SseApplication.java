ffd7491294fe6f18aa3415548acd5995eb997bc6
e1b20062T <e1b20062@oit.ac.jp>
2022-10-25 16:39:21 +0900

    ブラウザで指定のページを開いている限りカウントアップする数字を表示する処理を実装

--- src/main/java/oit/is/lec5/table1/sse/SseApplication.java
+++ src/main/java/oit/is/lec5/table1/sse/SseApplication.java
@@ -2,7 +2,10 @@ package oit.is.lec5.table1.sse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
 @SpringBootApplication
 public class SseApplication {
 
